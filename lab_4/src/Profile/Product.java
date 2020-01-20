859b0155267a7613211fecdcd88bbfa53b7b2911
AbhiThakkar <thakkar.ab@husky.neu.edu>
2020-01-19 20:10:14 -0500

    lab_4 uploaded

---
+++ lab_4/src/Profile/Product.java
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Profile;
+
+/**
+ *
+ * @author Abhi
+ */
+public class Product {
+    
+
+    private String firstname;
+    private String lastname;
+    private String ssn;
+    private String dob;
+    private String age;
+    private String phonenumber;
+    private String street;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String bankname1;
+    private String type1;
+    private String balance1;
+    private String account1;
+    private String routing1;
+    private String bankname2;
+    private String type2;
+    private String balance2;
+    private String account2;
+    private String routing2;
+    private String medicalrecord;
+    private String alergy1;
+    private String alergy2;
+    private String alergy3;
+    private String license;
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getBankname1() {
+        return bankname1;
+    }
+
+    public void setBankname1(String bankname1) {
+        this.bankname1 = bankname1;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    public String getBalance1() {
+        return balance1;
+    }
+
+    public void setBalance1(String balance1) {
+        this.balance1 = balance1;
+    }
+
+    public String getAccount1() {
+        return account1;
+    }
+
+    public void setAccount1(String account1) {
+        this.account1 = account1;
+    }
+
+    public String getRouting1() {
+        return routing1;
+    }
+
+    public void setRouting1(String routing1) {
+        this.routing1 = routing1;
+    }
+
+    public String getBankname2() {
+        return bankname2;
+    }
+
+    public void setBankname2(String bankname2) {
+        this.bankname2 = bankname2;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+    public String getBalance2() {
+        return balance2;
+    }
+
+    public void setBalance2(String balance2) {
+        this.balance2 = balance2;
+    }
+
+    public String getAccount2() {
+        return account2;
+    }
+
+    public void setAccount2(String account2) {
+        this.account2 = account2;
+    }
+
+    public String getRouting2() {
+        return routing2;
+    }
+
+    public void setRouting2(String routing2) {
+        this.routing2 = routing2;
+    }
+
+    public String getMedicalrecord() {
+        return medicalrecord;
+    }
+
+    public void setMedicalrecord(String medicalrecord) {
+        this.medicalrecord = medicalrecord;
+    }
+
+    public String getAlergy1() {
+        return alergy1;
+    }
+
+    public void setAlergy1(String alergy1) {
+        this.alergy1 = alergy1;
+    }
+
+    public String getAlergy2() {
+        return alergy2;
+    }
+
+    public void setAlergy2(String alergy2) {
+        this.alergy2 = alergy2;
+    }
+
+    public String getAlergy3() {
+        return alergy3;
+    }
+
+    public void setAlergy3(String alregy3) {
+        this.alergy3 = alregy3;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+    
+    
+    
+  }
+       
