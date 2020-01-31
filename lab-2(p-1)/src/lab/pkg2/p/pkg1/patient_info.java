6481b9d9111b027ca319e408f382f15031ac2378
AbhiThakkar <thakkar.ab@husky.neu.edu>
2020-01-30 22:22:39 -0500

    lab-2(p-1) uploaded

---
+++ lab-2(p-1)/src/lab/pkg2/p/pkg1/patient_info.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg2.p.pkg1;
+
+/**
+ *
+ * @author Abhi
+ */
+public class patient_info {
+      
+    int age_group;
+    int respiratory_rate;
+    int heart_rate;
+    int blood_pressure;
+    int weight_killos;
+    int weight_pounds;
+
+    public int getAge_group() {
+        return age_group;
+    }
+
+    public void setAge_group(int age_group) {
+        this.age_group = age_group;
+    }
+
+    public int getRespiratory_rate() {
+        return respiratory_rate;
+    }
+
+    public void setRespiratory_rate(int respirator_yrate) {
+        this.respiratory_rate = respirator_yrate;
+    }
+
+    public int getHeart_rate() {
+        return heart_rate;
+    }
+
+    public void setHeart_rate(int heart_rate) {
+        this.heart_rate = heart_rate;
+    }
+
+    public int getBlood_pressure() {
+        return blood_pressure;
+    }
+
+    public void setBlood_pressure(int blood_pressure) {
+        this.blood_pressure = blood_pressure;
+    }
+
+    public int getWeight_killos() {
+        return weight_killos;
+    }
+
+    public void setWeight_killos(int weight_killos) {
+        this.weight_killos = weight_killos;
+    }
+
+    public int getWeight_pounds() {
+        return weight_pounds;
+    }
+
+    public void setWeight_pounds(int weight_pounds) {
+        this.weight_pounds = weight_pounds;
+    }
+    
+    
+    
+    
+}
